f3c48f2cded06e9d641019c86815f26506cf37c5
Einsteinia11 <informationviakate2004@gmail.com>
2026-08-18 17:43:23 +0530

    Added rock paper scissors game

---
+++ Projects/Beginner/rockPaperScissors.py
@@ -0,0 +1,18 @@
+import random
+
+rock = 1
+paper = 2
+scissors = 3
+n = int(input("Enter 1 for rock 2 for paper 3 for scissors: "))
+c = random.randint(1, 3)
+print("You = ", n, " Computer = ", c)
+if (c == rock and n == 1) or (c == paper and n == 2) or (c==scissors and n == 3):
+    print("Match Draw as both are same")
+elif c == rock and (n == 2 or n == 3):
+    print("Computer won by rock")
+elif c == paper and (n == 1 or n == 3):
+    print("You won as computer thrown paper")
+elif c == scissors and n == 1:
+    print("You won as computer thrown scissors over rock")
+elif c == scissors and n == 2:
+    print("Computer won by throwing scissors over paper")
